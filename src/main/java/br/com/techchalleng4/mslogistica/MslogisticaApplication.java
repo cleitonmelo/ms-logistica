210071ce1cf1093bae07361c1caa1f40ff071d1f
Cleiton Melo Da Silva <cleiton.silva@gmmti.com.br>
2024-11-12 07:26:37 -0300

    Estrutura Inicial

---
+++ src/main/java/br/com/techchalleng4/mslogistica/MslogisticaApplication.java
@@ -0,0 +1,13 @@
+package br.com.techchalleng4.mslogistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MslogisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MslogisticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/techchalleng4/mslogistica/MslogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techchalleng4.mslogistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MslogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
